205b972e198647aa981648746be784b48a24def9
Anbu0011 <anbu.s2022cse@sece.ac.in>
2024-08-03 09:23:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const Productroutes = require("./routes/productRouters");
+const Userroutes = require("./routes/userRouters");
+const Cartroutes = require("./routes/cartRouter");
+const Orderroutes = require("./routes/orderRoute");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require("cors");
+require('dotenv').config();
+
+const MONGO_URI = process.env.MONGO_URI
+const PORT = process.env.PORT
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log("MongoDB Connected");
+    });
+
+app.set('view engine', "ejs");
+app.use(bodyParser.json())
+app.use("/", Productroutes, Userroutes, Cartroutes, Orderroutes)
+app.use(express.json);
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
